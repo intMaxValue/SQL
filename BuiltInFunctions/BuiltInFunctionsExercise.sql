baab34c543e26240c4ce359971fb9977e64848b1
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-09-26 12:41:52 +0300

    Update

--- BuiltInFunctions/BuiltInFunctionsExercise.sql
+++ BuiltInFunctions/BuiltInFunctionsExercise.sql
@@ -65,4 +65,64 @@ ORDER BY Salary DESC
 
 --GEOGRAPHY DATABASE
 
+SELECT CountryName, IsoCode
+FROM Countries
+WHERE CountryName LIKE '%a%a%a%'
+ORDER BY IsoCode
+
+
+
+SELECT PeakName, RiverName,
+	   LOWER(CONCAT(LEFT(P.PeakName, LEN(P.PeakName) -1) , r.RiverName)) AS Mix
+FROM Peaks AS P, Rivers AS R
+WHERE RIGHT(P.PeakName, 1) = LEFT(R.RiverName, 1)
+ORDER BY Mix
+
+
+--DIABLO DATABASE
+
+SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+FROM Games
+WHERE YEAR(Start) IN (2011, 2012)
+ORDER BY [Start], [Name] 
+
+
+SELECT Username,
+	   SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username 
+
+
+SELECT Username, IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+
+
+SELECT [Name] AS Game,
+    CASE 
+        WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+        WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+        WHEN DATEPART(HOUR, [Start]) >= 18 AND DATEPART(HOUR, [Start]) < 24 THEN 'Evening'
+    END AS [Part of the Day],
+
+	CASE 
+        WHEN Duration <= 3 THEN 'Extra Short'
+        WHEN Duration >= 4 AND Duration <= 6 THEN 'Short'
+		WHEN Duration > 6 THEN 'Long'
+		ELSE 'Extra Long'
+    END AS [Duration] 
+FROM Games
+ORDER BY Game, Duration
+
+
+--ORDERS DATABASE
+
+SELECT
+    ProductName,
+    OrderDate,
+    DATEADD(DAY, 3, OrderDate) AS PaymentDueDate,
+    DATEADD(MONTH, 1, OrderDate) AS DeliveryDueDate
+FROM
+    Orders;
 
