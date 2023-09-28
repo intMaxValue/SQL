8b8a1d97ceb1712897954b73df85e521444570b7
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-09-28 21:38:51 +0300

    SubqueriesAndJoins

---
+++ SubqueriesAndJoins/SubqueriesAndJoinsExercise.sql
@@ -0,0 +1,72 @@
+--SoftUni DB
+
+SELECT TOP 5 EmployeeID
+	   ,JobTitle
+	   ,a.AddressID
+	   ,a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a 
+ON e.AddressID = a.AddressID
+ORDER BY e.AddressID
+
+
+SELECT TOP 50 e.FirstName
+			 ,e.LastName
+			 ,t.[Name]
+			 ,a.AddressText
+FROM Employees AS e
+LEFT JOIN Addresses AS a ON e.AddressID = a.AddressID
+LEFT JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY e.FirstName, e.LastName
+
+
+
+SELECT e.EmployeeID
+		,e.FirstName
+		,e.LastName
+		,d.Name
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE d.DepartmentID = 3
+
+
+
+SELECT TOP 5 EmployeeID
+	   ,FirstName
+	   ,Salary
+	   ,d.[Name]
+FROM Employees AS e
+JOIN Departments AS D ON e.DepartmentID = D.DepartmentID
+WHERE Salary > 15000
+ORDER BY d.DepartmentID
+
+
+
+SELECT TOP 3 e.EmployeeID
+			 ,e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY EmployeeID
+
+
+
+SELECT FirstName
+	   ,LastName
+	   ,HireDate
+	   ,d.Name
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE HireDate > '1999-01-01' AND d.[Name] IN ('Sales', 'Finance')
+ORDER BY HireDate
+
+
+
+SELECT TOP 5 e.EmployeeID
+	   ,e.FirstName
+	   ,p.[Name]
+FROM Employees AS e
+JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+
